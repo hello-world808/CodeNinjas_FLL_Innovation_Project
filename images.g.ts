fd6c6b9a94083a62d4eaba3f3a81eda5679ca2f4
hello-world808 <jacktreick@gmail.com>
2025-11-04 17:13:34 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . e e e e e e . . . . . . 
+. . . e e e e e e e e . . . . . 
+. . . e e e e e e e e . . . . . 
+. . . . 4 4 4 4 4 4 . . . . . . 
+. . . . 4 f 5 5 f 4 . . . . . . 
+. . . . . f 5 5 f . . . . . . . 
+. . . . e 5 5 5 5 e . . . . . . 
+. . . . 2 e 2 2 e 2 . . . . . . 
+. . . . 5 1 e e 1 5 . . . . . . 
+. . . . 5 e e e e 5 . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . . f . . f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . e e e e e e . . . . . . 
+. . . e e e e e e e e . . . . . 
+. . . e e e e e e e e . . . . . 
+. . . . 4 4 4 4 4 4 . . . . . . 
+. . . . 4 f 5 5 f 4 . . . . . . 
+. . . . . f 5 5 f . . . . . . . 
+. . . . e 5 5 5 5 e . . . . . . 
+. . . . 2 e 2 2 e 2 . . . . . . 
+. . . . 5 1 e e 1 5 . . . . . . 
+. . . . 5 e e e e 5 . . . . . . 
+. . . . . e e e e . . . . . . . 
+. . . . . f . . f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -64,24 +64,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 })
 info.setScore(0)
 tiles.setCurrentTilemap(tilemap`level1`)
-let me = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . c c c c . . . . . . . . 
-    . . c c 5 5 5 5 c c . . . . . . 
-    . c 5 5 5 5 5 5 5 5 c . . . . . 
-    c 5 5 5 5 5 1 f 5 5 5 c . . . . 
-    c 5 5 5 5 5 f f 5 5 5 5 c . . . 
-    c c b b 1 b 5 5 5 5 5 5 c . . . 
-    c c 3 3 b b 5 5 5 5 5 5 d c . . 
-    c 5 3 3 3 5 5 5 5 5 d d d c . . 
-    . b 5 5 5 5 5 5 5 5 d d d c . . 
-    . . c b b c 5 5 b d d d d c . . 
-    . c b b c 5 5 b b d d d d c c c 
-    . c c c c c c d d d d d d d d c 
-    . . . c c c c d 5 5 b d d d c c 
-    . . . c b c c b 5 5 b c c c . . 
-    . . . c c c d 5 5 b c . . . . . 
-    `, SpriteKind.Player)
+let me = sprites.create(assets.image`myImage0`, SpriteKind.Player)
 tiles.placeOnTile(me, tiles.getTileLocation(26, 15))
 scene.cameraFollowSprite(me)
 controller.moveSprite(me)
